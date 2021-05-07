08ab555edb6d1dab71151da3222c2c23ec671066
lule ahmed <annetlwang2020@gmail.com>
2021-05-07 22:11:56 +0300

    patched ssl funtion using  express-sslify

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const enforce = require('express-sslify');
 
 const PORT =process.env.PORT || 5000;
 
-app.use(enforce.HTTPS(express.static(path.join(__dirname, 'public'))));
+app.use(enforce.HTTPS({trustProtoHeader:true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, ()=> console.log('server is listening on port 5000'));
