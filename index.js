a65f6e9551d595cd60bbae23017d67c101591a9e
lule ahmed <annetlwang2020@gmail.com>
2021-05-07 21:20:20 +0300

    added express-sslify to enforce https on all get requsets.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app =  express();
 const path = require('path');
+const enforce = require('express-sslify');
  
 
 const PORT =process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(enforce.HTTPS(express.static(path.join(__dirname, 'public'))));
 
 app.listen(PORT, ()=> console.log('server is listening on port 5000'));
