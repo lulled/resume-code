4b34f51ab2b8e8e74fe368d660d35accc3ac485f
Lule Ahmed <52065365+lulled@users.noreply.github.com>
2021-05-08 15:44:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app =  express();
 const path = require('path');
-const enforce = require('express-sslify');
+//const enforce = require('express-sslify');
  
 
 const PORT =process.env.PORT || 5000;
