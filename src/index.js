9fb9fb19b4545930f31f5152688270ff00957ddf
Lule Ahmed <52065365+lulled@users.noreply.github.com>
2022-02-04 15:19:17 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const PORT =process.env.PORT || 5000;
 
 //app.use(enforce.HTTPS({trustProtoHeader:true}));
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, ()=> console.log('server is listening on port 5000'));
